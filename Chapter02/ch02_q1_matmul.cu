fe0dc5241b2c71fd8b444ef3473230ceecd14f6a
zhouxin <zhou.xin2000@outlook.com>
2024-09-19 23:09:58 +0800

    finish ch02-q2-b column matmul

--- Chapter02/ch02_q1_matmul.cu
+++ Chapter02/ch02_q1_matmul.cu
@@ -18,6 +18,25 @@ __global__ void matmul_row_kernel(float *M, float *N, float *P, int width){
     }
 }
 
+__global__ void matmul_column_kernel(float *M, float *N, float *P, int width){
+    /*
+     * each thread compute one column of P
+     * both block and grid size are 1D
+     * i-th column of P involves the i-th column of N and all rows of M
+     */
+    int col = threadIdx.x + blockIdx.x * blockDim.x;
+    if(col < width){
+        for(int i=0; i<width; i++){
+            P[i*width+col] = 0;
+        }
+        for(int j=0; j<width; j++){ // j iterates over M's rows
+            for(int i=0; i<width; i++){ // i iterates over P's rows / N's rows
+                P[j*width+col] += M[j*width+i] * N[i*width+col];
+            }
+        }
+    }
+}
+
 extern "C" void matmul_row(float *M, float *N, float *P, int width){
     float *d_M, *d_N, *d_P;
     int size = width * width * sizeof(float);
@@ -30,6 +49,26 @@ extern "C" void matmul_row(float *M, float *N, float *P, int width){
     cudaMemcpy(d_N, N, size, cudaMemcpyHostToDevice);
 
     matmul_row_kernel<<<width, width>>>(d_M, d_N, d_P, width);
+
+    cudaMemcpy(P, d_P, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_M);
+    cudaFree(d_N);
+    cudaFree(d_P);
+}
+
+extern "C" void matmul_column(float *M, float *N, float *P, int width){
+    float *d_M, *d_N, *d_P;
+    int size = width * width * sizeof(float);
+
+    cudaMalloc(&d_M, size);
+    cudaMalloc(&d_N, size);
+    cudaMalloc(&d_P, size);
+
+    cudaMemcpy(d_M, M, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_N, N, size, cudaMemcpyHostToDevice);
+
+    matmul_column_kernel<<<width, width>>>(d_M, d_N, d_P, width);
 
     cudaMemcpy(P, d_P, size, cudaMemcpyDeviceToHost);
 
--- Chapter02/tests/test_ch02_q1_matmul.py
+++ Chapter02/tests/test_ch02_q1_matmul.py
@@ -3,9 +3,16 @@ import ctypes
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-cuda_lib = ctypes.CDLL(os.path.join(current_dir, "libch01_q1_matmul.so"))
+cuda_lib = ctypes.CDLL(os.path.join(current_dir, "libch02_q1_matmul.so"))
 
 cuda_lib.matmul_row.argtypes = [
+    np.ctypeslib.ndpointer(dtype=np.float32),
+    np.ctypeslib.ndpointer(dtype=np.float32),
+    np.ctypeslib.ndpointer(dtype=np.float32),
+    ctypes.c_uint
+    ]
+
+cuda_lib.matmul_column.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.float32),
     np.ctypeslib.ndpointer(dtype=np.float32),
     np.ctypeslib.ndpointer(dtype=np.float32),
@@ -21,6 +28,16 @@ def test_matmul_row():
 
     np.testing.assert_allclose(np.matmul(M, N), P, rtol=1e-3)
 
+def test_matmul_column():
+    width = 1000
+    M = np.random.rand(width, width).astype(np.float32)
+    N = np.random.rand(width, width).astype(np.float32)
+    P = np.random.rand(width, width).astype(np.float32)
+    cuda_lib.matmul_column(M, N, P, width)
+
+    np.testing.assert_allclose(np.matmul(M, N), P, rtol=1e-3)
+
 if __name__ == "__main__":
     test_matmul_row()
+    test_matmul_column()
     print("Pass")
